510eb80cafe1936255249e0fc5a174dc4098c403
Saebom Kwon <saebom@umich.edu>
2020-12-22 19:56:39 -0800

    Model, Router and Controller

---
+++ routers/userRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import routes from "../routes";
+import {
+  changePassword,
+  editProfile,
+  users,
+} from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
